4c4040f151ede4e956b6cd38058d8ddb5c277104
yuduki <rtccb.yuduki24@gmail.com>
2018-01-20 17:54:34 +0900

    クラス分割して画面を出すようにした

---
+++ src/View/View.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.locals import *
+import sys
+
+class View():
+    def __init__(self, scr_rect):
+        pygame.init()
+        self.screen = pygame.display.set_mode(scr_rect.size)
+        pygame.display.set_caption(u"uwaaaaaa")
+    def main(self):
+        self.draw(self.screen)
+        pygame.display.update()
+        self.key_handler()
+    def key_handler(self):
+        """キーハンドラー"""
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+    def draw(self, screen):
+        screen.fill((225, 150, 75))
